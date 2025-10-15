1c6a91c1d820567f114edd6283e0b4124f0dfcd3
Mael-Arcos <mael.arcos@soprasteria.com>
2025-10-15 17:05:46 +0200

    CustomForm : MaJ key et redirection

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import CategorySelect from "./CategorySelect";
+import { useNavigate } from "react-router";
 
 export default function CustomForm() {
   // useForm definition
@@ -9,7 +10,7 @@ export default function CustomForm() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-
+  const navigate = useNavigate();
   const [category, setCategory] = useState("0");
   const [difficulty, setDifficulty] = useState(null);
 
@@ -20,32 +21,43 @@ export default function CustomForm() {
     { id: "hard", name: "Hard" },
   ];
 
-  function onSubmit(data) {
-    // TODO
-  }
+  const onSubmit = () => {
+    // Redirect to the Quiz page with the selected parameters
+    navigate("/quiz", {
+      state: {
+        category: category,
+        difficulty: difficulty,
+      },
+    });
+  };
 
   return (
-    <form id="form" name="form" onSubmit={handleSubmit(onSubmit)}>
-      <CategorySelect
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-      />
-      <select
-        id="difficultySelect"
-        name="difficultySelect"
-        value={difficulty}
-        onChange={(e) => setDifficulty(e.target.value)}
-      >
-        <option value="">-- Sélectionnez une difficulté --</option>
-        {difficultyList.map((item) => (
-          <option key={item.id} value={item.id}>
-            {item.name}
+    <>
+      <h1>Quiz Maker</h1>
+      <form id="form" name="form" onSubmit={handleSubmit(onSubmit)}>
+        <CategorySelect
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <select
+          id="difficultySelect"
+          name="difficultySelect"
+          value={difficulty}
+          onChange={(e) => setDifficulty(e.target.value)}
+        >
+          <option key="dif" value="">
+            -- Sélectionnez une difficulté --
           </option>
-        ))}
-      </select>
-      <button id="createBtn" name="createBtn" type="submit">
-        Create
-      </button>
-    </form>
+          {difficultyList.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+        <button id="createBtn" name="createBtn" type="submit">
+          Create
+        </button>
+      </form>
+    </>
   );
 }
